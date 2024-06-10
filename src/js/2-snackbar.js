5ae17d8978b6bbf657496ab19931b77ff717348f
Olga Blud <olga.blud@gmail.com>
2024-06-10 16:40:56 +0300

    finish task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,7 +6,27 @@ import errorIcon from '../img/error.svg';
 const formElem = document.querySelector('.form');
 formElem.addEventListener('submit', onNotificationBtnSubmit);
 
-const makePromise = ({ delayMs, stateValue }) => {
+function onNotificationBtnSubmit(event) {
+  event.preventDefault();
+
+  const delayMs = Number(formElem.delay.value);
+  const stateValue = formElem.state.value;
+
+  if (delayMs < 0) {
+    iziToast.warning(getMessageObj('warning'));
+    formElem.reset();
+    return;
+  }
+
+  const promise = makePromise({ delayMs, stateValue });
+  promise
+    .then(delayMs => iziToast.success(getMessageObj('success', delayMs)))
+    .catch(delayMs => iziToast.error(getMessageObj('error', delayMs)));
+
+  formElem.reset();
+}
+
+function makePromise({ delayMs, stateValue }) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (stateValue === 'fulfilled') {
@@ -16,99 +36,44 @@ const makePromise = ({ delayMs, stateValue }) => {
       }
     }, delayMs);
   });
-};
+}
 
-function onNotificationBtnSubmit(event) {
-  event.preventDefault();
-
-  const delayMs = Number(formElem.delay.value) * 1000;
-  if (delayMs < 0) {
-    iziToast.warning({
-      title: 'Caution',
-      message: `Make choice more than 0`,
-      backgroundColor: '#ffa000',
-      iconUrl: successIcon,
-      theme: 'dark',
-      position: 'topRight',
-      messageColor: '#ffffff',
-    });
-    formElem.reset();
-    return;
-  }
-
-  const stateValue = formElem.state.value;
-  const promise = makePromise({ delayMs, stateValue });
-  promise
-    // .then(delayMs => console.log(`✅ Fulfilled promise in ${delayMs}ms`))
-    // .catch(delayMs => console.log(`❌ Rejected promise in ${delayMs}ms`));
-    .then(delayMs =>
-      iziToast.success({
+function getMessageObj(messageState, delayMs = 0) {
+  const commonSettings = {
+    theme: 'dark',
+    position: 'topRight',
+    messageColor: '#ffffff',
+  };
+  switch (messageState) {
+    case 'success':
+      return {
         title: 'OK',
         message: `Fulfilled promise in ${delayMs}ms`,
         backgroundColor: '#59a10d',
         iconUrl: successIcon,
-        theme: 'dark',
-        position: 'topRight',
-        messageColor: '#ffffff',
-      })
-    )
-    .catch(delayMs =>
-      iziToast.error({
+        ...commonSettings,
+      };
+    case 'error':
+      return {
         title: 'Error',
         message: `Rejected promise in ${delayMs}ms`,
         backgroundColor: '#ef4040',
         iconUrl: errorIcon,
-        theme: 'dark',
-        position: 'topRight',
-        messageColor: '#ffffff',
-      })
-    );
-
-  /* const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (stateValue === 'fulfilled') {
-        resolve(delayMs);
-      } else {
-        reject(delayMs);
-      }
-    }, delayMs);
-  });
-
-  promise
-    .then(delayMs => console.log(`✅ Fulfilled promise in ${delayMs}ms`))
-    .catch(error => {
-      console.log(`❌ Rejected promise in ${delayMs}ms`);
-    }); */
-
-  formElem.reset();
+        ...commonSettings,
+      };
+    case 'warning':
+      return {
+        title: 'Caution',
+        message: 'Make choice more than 0',
+        backgroundColor: '#ffa000',
+        ...commonSettings,
+      };
+    default:
+      return {
+        title: 'Error',
+        message: 'Unknown problem',
+        backgroundColor: '#ef4040',
+        ...commonSettings,
+      };
+  }
 }
-
-// const iziToastMessages = {
-//   successMessage: delayMs => ({
-//     title: 'OK',
-//     message: `Fulfilled promise in ${delayMs}ms`,
-//     backgroundColor: '#59a10d',
-//     iconUrl: successIcon,
-//     theme: 'dark',
-//     position: 'topRight',
-//     messageColor: '#ffffff',
-//   }),
-//   errorMessage: delayMs => ({
-//     title: 'Error',
-//     message: `Rejected promise in ${delayMs}ms`,
-//     backgroundColor: '#ef4040',
-//     iconUrl: errorIcon,
-//     theme: 'dark',
-//     position: 'topRight',
-//     messageColor: '#ffffff',
-//   }),
-//   warningMessage: delayMs => ({
-//     title: 'Caution',
-//     message: `Make choice more than 0`,
-//     backgroundColor: '#ffa000',
-//     iconUrl: successIcon,
-//     theme: 'dark',
-//     position: 'topRight',
-//     messageColor: '#ffffff',
-//   }),
-// };
